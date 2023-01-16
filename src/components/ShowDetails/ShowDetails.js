8d1b4dfe3f5b1ef66d420f5331f59c6aa17273ed
ishrafil2233 <ishrafil2233@gmail.com>
2023-01-16 15:42:46 +0600

    update details cart

--- src/components/ShowDetails/ShowDetails.js
+++ src/components/ShowDetails/ShowDetails.js
@@ -6,15 +6,15 @@ const ShowDetails = () => {
     const { idMeal, strMealThumb, strMeal, strInstructions } = sigleMeal;
     return (
         <div >
-            <div className='grid lg:grid-cols-2 gap-x-12 md:mx-12 md:pt-10 justify-center align-middle border my-5'>
-                <div className='grid justify-items-center'>
+            <div className='grid lg:grid-cols-2 md:mx-12 md:pt-10 justify-center align-middle mb-5'>
+                <div>
                     <img className='md:rounded-xl' src={strMealThumb} alt="" />
                 </div>
-                <div className='mx-3'>
+                <div className='md:mx-3 bg-[#86efac] p-5 md:rounded-xl grid content-center text-start'>
                     <div>
-                        <h1>ID: {idMeal}</h1>
-                        <h3>Title: {strMeal}</h3>
-                        <p className='text-justify'>Description: {strInstructions}</p>
+                        <h1 className='text-2xl font-semibold'>ID: {idMeal}</h1>
+                        <h3 className='text-2xl font-semibold'>Title: {strMeal}</h3>
+                        <p className='text-justify'><span className='font-semibold text-lg'>Description:</span> {strInstructions}</p>
                     </div>
                 </div>
             </div>
